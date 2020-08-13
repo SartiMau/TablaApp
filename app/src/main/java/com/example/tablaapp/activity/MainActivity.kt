5fc3cee4740b4358af5f6503b90edb70a43044cb
Mauro Sarti <mauro.sarti@globant.com>
2020-08-13 13:35:11 -0300

    added toolbar

--- app/src/main/java/com/example/tablaapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/tablaapp/activity/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.tablaapp.activity
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import com.example.tablaapp.ui.setToolbar
+import com.example.tablaapp.ui.showDialogAddNewPlayer
 import com.example.tablaapp.viewmodel.MainViewModel
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.CONFIRMATION_ERROR
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.FINISH
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.INIT
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.SHOW_DIALOG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +24,43 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            updateUI()
+        }
+    }
+
+    @Composable
+    private fun updateUI() {
+        applicationContext.let {
+            when (viewModel.mainState.value.state) {
+                INIT -> init(it)
+                SHOW_DIALOG -> showDialogAddNewPlayer(it)
+                FINISH -> finish()
+                CONFIRMATION_ERROR -> showDialogAddNewPlayer(it)
+            }
+        }
+    }
+
+    @Composable
+    private fun init(context: Context) {
+        Column {
+            setToolbar(context, viewModel)
+            showMainScreenContent()
+        }
+    }
+
+    @Composable
+    private fun showDialogAddNewPlayer(context: Context) {
+        Column {
+            setToolbar(context, viewModel)
+            showDialogAddNewPlayer(context, viewModel)
+            showMainScreenContent()
+        }
+    }
+
+    @Composable
+    private fun showMainScreenContent() {
+        Text(viewModel.mainState.value.name)
     }
 }
--- app/src/main/java/com/example/tablaapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/tablaapp/viewmodel/MainViewModel.kt
@@ -1,17 +1,67 @@
 package com.example.tablaapp.viewmodel
 
+import androidx.compose.MutableState
+import androidx.compose.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.ui.graphics.Color
+import com.example.tablaapp.util.EMPTY_STRING
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.FINISH
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.INIT
+import com.example.tablaapp.viewmodel.MainViewModel.MainStatus.SHOW_DIALOG
 
 class MainViewModel @ViewModelInject constructor() : ViewModel() {
 
-    private var mutableMainState: MutableLiveData<MainData> = MutableLiveData()
-    val mainState: LiveData<MainData>
+    private var mutableMainState: MutableState<MainData> = mutableStateOf(MainData(INIT, EMPTY_STRING, MainDialogData()))
+
+    val mainState: MutableState<MainData>
         get() = mutableMainState
 
-    data class MainData(val status: MainStatus)
 
-    enum class MainStatus
+    fun showDialogAddNewPlayer() {
+        mainState.value.let {
+            mutableMainState.value = MainData(SHOW_DIALOG, it.name, MainDialogData(it.dialogData.isEmptyDialogInputText))
+        }
+    }
+
+    fun onNavigationIconClicked() {
+        mutableMainState.value = MainData(FINISH, EMPTY_STRING, MainDialogData())
+    }
+
+    fun closeDialog() {
+        mainState.let {
+            mutableMainState.value = MainData(INIT, it.value.name, MainDialogData())
+        }
+    }
+
+    fun onConfirmDialogButton(textFieldStatus: String) {
+        mainState.value.let {
+            if (textFieldStatus.isEmpty()) {
+                mutableMainState.value = MainData(
+                    SHOW_DIALOG, it.name, MainDialogData(isEmptyDialogInputText = true, showErrorText = true, labelColor = Color.Red)
+                )
+            } else {
+                mutableMainState.value = MainData(INIT, textFieldStatus, MainDialogData())
+            }
+        }
+    }
+
+    data class MainData(
+        val state: MainStatus,
+        val name: String,
+        val dialogData: MainDialogData
+    )
+
+    data class MainDialogData(
+        val isEmptyDialogInputText: Boolean = false,
+        val showErrorText: Boolean = false,
+        val labelColor: Color = Color.Unset
+    )
+
+    enum class MainStatus {
+        INIT,
+        SHOW_DIALOG,
+        FINISH,
+        CONFIRMATION_ERROR
+    }
 }
